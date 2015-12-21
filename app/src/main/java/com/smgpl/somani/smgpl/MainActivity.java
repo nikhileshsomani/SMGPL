509bc86780da3c687d1727a2ebd346d928073f26
nikhileshsomani <nikhilesh.somani@gmail.com>
2015-12-21 23:58:17 +0530

    start

---
+++ app/src/main/java/com/smgpl/somani/smgpl/MainActivity.java
@@ -0,0 +1,56 @@
+package com.smgpl.somani.smgpl;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Context self = this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button addMeasurement = (Button) findViewById(R.id.addButton);
+        addMeasurement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder details = new AlertDialog.Builder(self);
+                details.setTitle("Measurement Details");
+                LayoutInflater li = LayoutInflater.from(self);
+                View detailsView =  li.inflate(R.layout.measurements,null);
+                details.setView(detailsView);
+                AlertDialog getDetails = details.create();
+                getDetails.show()
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
